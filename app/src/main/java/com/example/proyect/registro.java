952612fac55c8bd10b44b3fada88682199ab3237
AlejandraBarco <a.barcob@alumnos.upm.es>
2023-03-23 18:54:51 +0100

    He añadido la funcionalidad de pulsar los botones de inicio de sesión y registro y que te lleven a su activity correspondiente

--- app/src/main/java/com/example/proyect/registro.java
+++ app/src/main/java/com/example/proyect/registro.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class login extends AppCompatActivity {
+public class registro extends AppCompatActivity {
 
     private Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_registro);
         buttonLogin = findViewById(R.id.login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(login.this,registro.class));
+                //meter aqui dentro lo de if task.succesful del firebase
+                startActivity(new Intent(registro.this,login.class));
             }
         });
     }
-
 }
